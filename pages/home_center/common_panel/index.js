bcebaf34255cef449c5013996074701967da858b
Xiaole Tao <xiaolet@cisco.com>
2021-05-16 18:14:23 +0800

    update

--- pages/home_center/common_panel/index.js
+++ pages/home_center/common_panel/index.js
@@ -1,10 +1,10 @@
-// miniprogram/pages/home_center/common_panel/index.js.js
-import {
-    getDevFunctions,
-    getDeviceDetails,
-    deviceControl
-} from '../../../utils/api/device-api'
-import wxMqtt from '../../../utils/mqtt/wxMqtt'
+import { getDevFunctions, getDeviceDetails, deviceControl } from '../../../utils/api/device-api';
+import { getStatiHours, getStatiDays, getStatiMonths, getStatiAll, getStatiType } from '../../../utils/api/statistics-api';
+import wxMqtt from '../../../utils/mqtt/wxMqtt';
+
+let wxCharts = require('../../../utils/wxcharts.js');
+let columnChart = null;
+
 let {
     powerOn,
     powerOff,
@@ -13,11 +13,9 @@ let {
     timer,
     bgImage,
     statistics
-} = require('./img')
-
+} = require('./img');
 
 Page({
-
     /**
      * 页面的初始数据
      */
@@ -222,15 +220,207 @@ Page({
 
     //获得当前设备状态
     turnDeviceOn: function(e) {
-        const {
-            value
-        } = this.data.rwDpList.switch;
-        this.sendDp('switch', !value)
+        const switcher = this.data.rwDpList.switch,
+            value = switcher ? switcher.value : "undefined";
+        if (value !== "undefined") {
+            this.sendDp('switch', !value)
+        }
     },
 
     //待开发功能
     turnNoticeOn: function() {
         this.toast.showToast('功能待开发~');
-    }
+    },
 
-})
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: async  function () {
+    const { device_id } = this.data
+    //const { thisDay,sum,years }  = await getStatiAll(device_id)
+    const thisDaystartHour = this.formatTime(new Date().getTime(),'YMD') + '00'
+    const thisDayendHour = this.formatTime(new Date().getTime(),'YMD') + '23'
+    const thisDay  = await getStatiAll(device_id)
+    let thisMonstart = this.formatTime(new Date().getTime(),'YM')
+    const thisMon  = await getStatiMonths(device_id, thisMonstart, thisMonstart)
+    const statiHours  = await getStatiHours(device_id, thisDaystartHour, thisDayendHour)
+    this.setData({ 
+      list_data:thisDay,
+    })
+    this.setData({ 
+      baobiao_data:statiHours.hours,
+    })
+    let { months } = thisMon
+    this.setData({ 
+      year_data:months,
+      thisdayString:this.formatTime(new Date().getTime(),'YMD'),
+      thismonString:this.formatTime(new Date().getTime(),'YM'),
+    })
+    let windowWidth = 300;
+        try {
+            let res = wx.getSystemInfoSync();
+            windowWidth = res.windowWidth - 30;
+        } catch (options) {
+            console.error('getSystemInfoSync failed!');
+        }
+        
+        let simulationData = this.createSimulationData(statiHours.hours);
+        columnChart = new wxCharts({
+            canvasId: 'columnCanvas',
+            type: 'column',
+            categories: simulationData.categories,
+            animation: true,
+            series: [{
+                name: '用电量',
+                data: simulationData.data,
+                format: function (val, name) {
+                    return val + 'kwh';
+                }
+            }],
+            xAxis: {
+                disableGrid: true
+            },
+            yAxis: {
+                title: '用电量(kwh)',
+                format: function (val) {
+                    return val; 
+                },
+                min: 0
+            },
+            width: windowWidth,
+            height: 260,
+            dataLabel: false,
+            dataPointShape: true,
+            extra: {
+                lineStyle: 'straight'
+            }
+        });
+    },
+
+    byHour: async function() {
+        let {
+            device_id
+        } = this.data
+        let dateTime = this.formatTime(new Date().getTime(), 'YMD')
+        let thisstartHour = dateTime + '00'
+        let thisendHour = dateTime + '23'
+        let statiHours = await getStatiHours(device_id, thisstartHour, thisendHour)
+        this.updateData(statiHours.hours);
+        //this.setData({
+        //baobiao_data:statiHours.hours,
+        //});
+        this.setData({
+            ribaodate: dateTime,
+        });
+    },
+
+    byDay: async function() {
+        let {
+            device_id
+        } = this.data
+        let dateTime = this.formatTime(new Date().getTime(), 'YM')
+        let thisstartDay = dateTime + '01'
+        let thisendDay = dateTime + '31'
+        let statiDays = await getStatiDays(device_id, thisstartDay, thisendDay)
+
+        this.setData({
+            yuebaodate: dateTime,
+        });
+        this.updateData(statiDays.days);
+    },
+
+    byMonth: async function() {
+        let dateTime = new Date().getFullYear()
+        let {
+            device_id
+        } = this.data
+        let thisstartMon = dateTime + '01'
+        let thisendMon = dateTime + '12'
+        let statiMons = await getStatiMonths(device_id, thisstartMon, thisendMon)
+
+        this.setData({
+            nianbaodate: dateTime,
+        });
+        this.updateData(statiMons.months);
+    },
+
+    formatNumber(n) {
+        n = n.toString()
+        return n[1] ? n : '0' + n
+    },
+
+    formatTime(number, format) {
+        let formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+        let returnArr = [];
+
+        let date = new Date(number);
+        returnArr.push(date.getFullYear());
+        returnArr.push(this.formatNumber(date.getMonth() + 1));
+        returnArr.push(this.formatNumber(date.getDate()));
+
+        for (let i in returnArr) {
+            format = format.replace(formateArr[i], returnArr[i]);
+        }
+        return format;
+    },
+
+    touchHandler: function(e) {
+        columnChart.scrollStart(e);
+    },
+
+    moveHandler: function(e) {
+        columnChart.scroll(e);
+    },
+
+    touchEndHandler: function(e) {
+        columnChart.scrollEnd(e);
+        columnChart.showToolTip(e, {
+            format: function(item, category) {
+                return category + ' ' + item.name + ':' + item.data
+            }
+        });
+    },
+    makeCategorySense: function(s) {
+        if (s.length === 6) {
+            return s.replace(/^.+(..)$/, "$1月");
+        } else if (s.length === 8) {
+            return s.replace(/^.+(..)$/, "$1日");
+        } else if (s.length === 10) {
+            return s.replace(/^.+(..)$/, "$1时");
+        }
+        return s;
+    },
+
+    createSimulationData: function(e) {
+        //将hours的Json数据转换为图表用的索引数组和数值数组
+        let arr = [];
+        let yourdata = e;
+        let arri = Object.keys(yourdata).map(this.makeCategorySense); //这是索引数组2021042500-23
+        for (let one in yourdata) {
+            let str = yourdata[one];
+            //加到数组中去
+            arr.push(str); //这是对应的数值数组
+        }
+        console.log(arri);
+        return {
+            categories: arri,
+            data: arr
+        };
+    },
+
+    updateData: function(e) {
+        let simulationData = this.createSimulationData(e);
+        let series = [{
+            name: '用电量',
+            data: simulationData.data,
+            format: function(val, name) {
+                return val + 'kwh';
+            }
+        }];
+        columnChart.updateData({
+            categories: simulationData.categories,
+            series: series
+        });
+    }
+});
